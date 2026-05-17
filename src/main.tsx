7f971ffbe93fa5db59436f23f84f9cc559be77d0
adhamahmed23A <domaaaadham@gmail.com>
2026-05-17 06:54:32 -0700

    The source of project

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import { ThemeProvider } from "./providers/ThemeProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// new instance from query client
 
 export const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <App />
-      </ThemeProvider>
+      <App />
     </BrowserRouter>
   </QueryClientProvider>,
 );
--- src/utils/describtionInCard.util.ts
+++
@@ -1,6 +0,0 @@
-export const handleDescribtion = (text: string) => {
-  const splittedText = text.split(" ");
-  return splittedText.length > 30
-    ? `${splittedText.slice(0, 30).join(" ")} ...`
-    : text;
-};
